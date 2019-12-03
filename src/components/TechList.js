add6a5cd0585c2732ac13b27569f89e74ce8398a
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2019-12-03 00:29:16 -0300

    Removendo itens do estado

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -19,12 +19,23 @@ class TechList extends Component {
     });
   };
 
+  handleDelete = currTech => {
+    this.setState({
+      techs: this.state.techs.filter(tech => tech !== currTech)
+    });
+  };
+
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <ul>
           {this.state.techs.map(tech => (
-            <li key={tech}>{tech}</li>
+            <li key={tech}>
+              {tech}
+              <button onClick={() => this.handleDelete(tech)} type="button">
+                Remover
+              </button>
+            </li>
           ))}
         </ul>
         <input
